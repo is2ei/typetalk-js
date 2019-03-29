2d2e6a38a4195072a94a9578ac6dc9be548665ca
Issei Horie <issei.horie@nulab.com>
2019-03-29 09:09:07 +0900

    Absolute url

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ const hostName = "typetalk.com";
 
 exports.Endpoints = {
     // https://developer.nulab.com/docs/typetalk/api/1/post-message/
-    PostMessage: (topicID) => `${hostName}/api/v1/topics/${topicID}`
+    PostMessage: (topicID) => `https://${hostName}/api/v1/topics/${topicID}`
 };
