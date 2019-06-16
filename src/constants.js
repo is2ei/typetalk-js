d0b1701642c4454e1bbc601889ab90325f8596a7
Issei Horie <is2ei.horie@gmail.com>
2019-06-16 20:27:49 +0900

    Added method 'getAccessToken'

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,9 @@
 const hostName = "typetalk.com";
 
 exports.Endpoints = {
+    // https://developer.nulab.com/docs/typetalk/auth/
+    getAccessToken: () => `https://${hostName}/oauth2/access_token`,
+
     // https://developer.nulab.com/docs/typetalk/api/1/post-message/
     postMessage: (topicID) => `https://${hostName}/api/v1/topics/${topicID}`,
 
